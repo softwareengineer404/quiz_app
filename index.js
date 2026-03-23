6b1803fa566f637ae6b7c1e09b411e7607c6dbc2
tesfay <tesfayhailemariam29@gmail.com>
2026-03-23 23:07:54 +0300

    add options

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const questions = [
         question: "which is the largest animal in the world?",
         answers: [
             { text: "shark", correct: false},
-            { text: "blue whale", correct: true}
+            { text: "blue whale", correct: true},
+            { text: "elephant", correct: false},
+            { text: "giraffe", correct: false}
         ]
     },
 ]
