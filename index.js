33da5af53adbafde69188f17a59ec66fcbb6f0d9
tesfay <tesfayhailemariam29@gmail.com>
2026-03-23 22:59:18 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const questions = [
+    {
+        question: "which is the largest animal in the world?",
+        answers: [
+            { text: "shark", correct: false},
+            { text: "blue whale", correct: true}
+        ]
+    },
+]
