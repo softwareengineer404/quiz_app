1abf102cbee10de634e91369d117d4c0dce5f1a7
tesfay <tesfayhailemariam29@gmail.com>
2026-03-24 21:09:13 +0300

    js functionality

--- index.js
+++ index.js
@@ -18,4 +18,9 @@ const questions = [
 
         ]
     }
-]
+];
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+let currentQuestionIndex = 0;
+let score = 0;
