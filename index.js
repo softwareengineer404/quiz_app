086b1b36219a3835e65e7c325fff2bcb8ac329e8
tesfay <tesfayhailemariam29@gmail.com>
2026-03-24 22:05:23 +0300

    changes

--- index.js
+++ index.js
@@ -49,14 +49,23 @@ function startQuiz() {
     showQuestion();
 }
 function showQuestion() {
+    resetStste();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
     questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
 
-    currentQuestion.answers.foeEach(answer  => {
+    currentQuestion.answers.forEach(answer  => {
         const button = document.createElement("button");
         button.innerHTML = answer.text;
         button.classList.add("btn");
         answerButtons.appendChild(button);
-    })
+        
+    });
+}
+startQuiz();
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
 }
