27f0931724e3f700a07b5feafe208339c104ec4d
tesfay <tesfayhailemariam29@gmail.com>
2026-03-24 21:34:34 +0300

    add showquestion function

--- index.js
+++ index.js
@@ -42,3 +42,14 @@ const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 let currentQuestionIndex = 0;
 let score = 0;
+function startQuiz() {
+    currentQuestionIndexQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+function showQuestion() {
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+}
